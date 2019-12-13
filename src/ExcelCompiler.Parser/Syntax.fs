4719e84434689a14b5988c70cb46430a408d5002
Taylor Southwick <twsouthwick@outlook.com>
2019-12-13 11:38:04 -0800

    Create single lists when possible

--- src/ExcelCompiler.Parser/Syntax.fs
+++ src/ExcelCompiler.Parser/Syntax.fs
@@ -12,7 +12,7 @@ type  SyntaxList<'T when 'T : equality>  =
     override this.Equals(y) =
         match y with
         | :? SyntaxList<'T> as other ->
-            match (this.unwrap this, this.unwrap other) with
+            match (this, other) with
             | (Empty, Empty) -> true
             | (Single s1, Single s2) -> s1.Equals(s2)
             | (List l1, List l2) -> Enumerable.SequenceEqual(l1, l2)
@@ -27,11 +27,6 @@ type  SyntaxList<'T when 'T : equality>  =
             let x = new HashCode()
             for i in l do x.Add(i)
             x.ToHashCode()
-
-     member private this.unwrap x =
-            match x with
-            | List l -> if l.Count() > 1 then x else Single(l.First())
-            | _ -> x
 
 type Factor =
     | Int of int
@@ -48,3 +43,7 @@ type Formula =
     | Expression of Expression
     | EmptyExpression
 
+let internal CreateList<'T when 'T : equality> (t : 'T list) =
+    if t.IsEmpty then Empty
+    elif t.Length = 1 then Single(List.head t)
+    else List(t)
