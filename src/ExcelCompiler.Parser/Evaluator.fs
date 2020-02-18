700587d70efd897b731918e320054d8de80b942b
Taylor Southwick <twsouthwick@outlook.com>
2020-02-18 11:24:48 -0800

    Simple evaluator works

--- src/ExcelCompiler.Parser/Evaluator.fs
+++ src/ExcelCompiler.Parser/Evaluator.fs
@@ -11,24 +11,50 @@ let public GetIntValue statement =
                          | _ -> 0
         | _ -> 0
 
-let Binary left op right =
-    Number(Int(5))
+let private BinaryFloat (left : float) op (right : float) =
+    match op with
+    | Add -> left + right
+    | Subtract -> left - right
+    | Multiply -> left * right
+    | Divide -> left * right
 
-let Function f exp =
-    Number(Int(5))
+let private BinaryInt left op right =
+    match op with
+    | Add -> left + right
+    | Subtract -> left - right
+    | Multiply -> left * right
+    | Divide -> left * right
 
-let Unwrap exp =
+let private BinaryNumber left op right =
+    match (left, right) with
+    | (Int l,  Int r) -> Int(BinaryInt l op r)
+    | (Int l,  Float r) -> Float(BinaryFloat (float l) op r)
+    | (Float l,  Int r) -> Float(BinaryFloat l op (float r))
+    | (Float l,  Float r) -> Float(BinaryFloat l op r)
+
+let private BinaryLiteral left op right =
+    match (left, right) with
+    | (Number l, Number r) -> Number(BinaryNumber l op r)
+    | (String l, String r) -> String(l + r)
+    | (_, _) -> failwith "Invalid literal binary operation"
+
+let private Binary left op right =
+    match (left, right) with
+    | (LiteralExpression l, LiteralExpression r) -> Literal(BinaryLiteral l op r)
+    | (_, _) -> failwith "Unknown"
+
+let private Unwrap exp =
     match exp with
-    | LiteralExpression l -> l
+    | LiteralExpression l -> Literal(l)
     | BinaryExpression (left, op, right) -> Binary left op right
-    | FunctionExpression (f, exp) -> Function f exp
-    | _ -> Number(Int(0))
+    //| FunctionExpression (f, exp) -> Function f exp
+    | _ -> Literal(Number(Int(0)))
 
 let public EvaluateSyntax compiledDocument cell : Statement =
     let syntax : Statement = compiledDocument(cell)
 
     match syntax with
-        | Formula f -> Literal (Unwrap f)
+        | Formula f -> Unwrap f
         | Literal l -> Literal l
         | Text t -> Text t
         | Nothing -> Nothing
---
+++ tests/ExcelCompiler.Tests/EvaluatorTests.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Xunit;
+
+using static ExcelCompiler.Syntax;
+
+namespace ExcelCompiler.Tests
+{
+    public class EvaluatorTests
+    {
+        [Fact]
+        public void EmptyLiteral()
+        {
+            var doc = new TestCompiledDocument();
+            var evaluator = new SyntaxEvaluator(doc);
+
+            Assert.Equal(0, evaluator.GetCell<int>("A1"));
+        }
+
+        [Fact]
+        public void SingleLiteral()
+        {
+            var doc = new TestCompiledDocument
+            {
+                { "A1", Statement.NewLiteral(Literal.NewNumber(Number.NewInt(2))) }
+            };
+
+            var evaluator = new SyntaxEvaluator(doc);
+
+            Assert.Equal(2, evaluator.GetCell<int>("A1"));
+        }
+
+        [Fact]
+        public void SingleBinaryAddition()
+        {
+            var doc = new TestCompiledDocument
+            {
+                { "A1", Statement.NewFormula(
+                    Expression.NewBinaryExpression(
+                        Expression.NewLiteralExpression(
+                            Literal.NewNumber(Number.NewInt(2))),
+                        Operation.Add,
+                        Expression.NewLiteralExpression(
+                            Literal.NewNumber(Number.NewInt(3)))))}
+            };
+
+            var evaluator = new SyntaxEvaluator(doc);
+
+            Assert.Equal(5, evaluator.GetCell<int>("A1"));
+        }
+
+        private class TestCompiledDocument : ICompiledDocument, IEnumerable<(CellReference, Syntax.Statement)>
+        {
+            private readonly Dictionary<CellReference, Syntax.Statement> _lookup = new Dictionary<CellReference, Statement>();
+
+            public void Add(CellReference reference, Syntax.Statement statement)
+            {
+                _lookup.Add(reference, statement);
+            }
+
+            public Syntax.Statement GetCell(CellReference cell)
+            {
+                if (_lookup.TryGetValue(cell, out var result))
+                {
+                    return result;
+                }
+
+                return Syntax.Statement.Nothing;
+            }
+
+            IEnumerator<(CellReference, Syntax.Statement)> IEnumerable<(CellReference, Syntax.Statement)>.GetEnumerator()
+            {
+                foreach (var item in _lookup)
+                {
+                    yield return (item.Key, item.Value);
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => _lookup.GetEnumerator();
+        }
+    }
+}
